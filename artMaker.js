9cc2a5da1dc9dab5490ea5ed1da6f87c1971fe03
Tyler Conway <tylerconway@stripe.com>
2020-06-15 16:22:01 -0700

    drag

--- artMaker.js
+++ artMaker.js
@@ -1,4 +1,16 @@
 window.onload = function(){
+    
+     //globally listens for mouse downness
+     let mouseIsDown = false;
+
+     window.addEventListener('mousedown', function(){
+         mouseIsDown = true;
+     });
+ 
+     window.addEventListener('mouseup', function(){
+         mouseIsDown = false;
+     });
+    
     //Create main grid
     let container = document.querySelector('#canvas');
     container.style.gridTemplate = 'repeat(20, 1fr) / repeat(20, 1fr)'
@@ -14,6 +26,7 @@ window.onload = function(){
 
     //define active color
     let activeColor = 'red';
+
 
     let clickBoxes = document.querySelectorAll('span');
     //for each element in this array, add event listener and change to active color on click.
@@ -36,16 +49,25 @@ window.onload = function(){
     for(let i = 0; i < 6; i++) {
         let palletBox = document.createElement('span');
         palletBox.style.backgroundColor = colorChoices[i];
+
+        palletBox.addEventListener('mouseenter', function() {
+            if (mouseIsDown) 
+            {palletBox.style.backgroundColor = activeColor;
+            }})
     
     //if particular palletBox is clicked, change the activeColor to whatever background color was clicked
     palletBox.addEventListener('click', function(event) {
         activeColor = palletBox.style.backgroundColor;
         })
+
+
     
     //if custom color is chosen, change the activeColor to whatever background color was clicked
     palletPicker.addEventListener('click', function(event) {
         activeColor = palletPicker.style.backgroundColor;
         })    
+
+
 
     palletContainer.appendChild(palletBox);
     palletPicker.appendChild(palletContainer);
